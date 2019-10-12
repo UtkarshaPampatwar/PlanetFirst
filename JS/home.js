513046a8dd50ddac29969360c1ef14e46a148077
utkarshapampatwar <pampatwar.u@husky.neu.edu>
2019-10-12 14:05:24 -0400

    Added the script in the seperate file

--- JS/home.js
+++ JS/home.js
@@ -1,3 +1,38 @@
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').focus()
 })
+
+var touchSensitivity = 5;
+$(".carousel").on("touchstart", function (event) {
+    var xClick = event.originalEvent.touches[0].pageX;
+    $(this).one("touchmove", function (event) {
+        var xMove = event.originalEvent.touches[0].pageX;
+        if (Math.floor(xClick - xMove) > touchSensitivity) {
+            $(this).carousel('next');
+        } else if (Math.floor(xClick - xMove) < -(touchSensitivity)) {
+            $(this).carousel('prev');
+        }
+    });
+    $(".carousel").on("touchend", function () {
+        $(this).off("touchmove");
+    });
+});
+
+(function ($) {
+    "use strict";
+
+    // manual carousel controls
+    $('.next').click(function () {
+        $('.carousel').carousel('next');
+        return false;
+    });
+    $('.prev').click(function () {
+        $('.carousel').carousel('prev');
+        return false;
+    });
+
+})(jQuery);
+
+function mySubmit() {
+    alert("Thank you for your feedback! We're always improving.");
+}
